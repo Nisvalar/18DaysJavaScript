6242cfbbf58187e35e5cd2c258b8523452bd4858
Nisvalar <jose_david_51@hotmail.com>
2025-03-11 06:14:26 +0100

    Subida del dia 14 - Heroku

---
+++ JavaScript 18 Days/Day14 - Aplicaciones Web/6.- Heroku/script.js
@@ -0,0 +1,28 @@
+/**
+ * SUBIDA DEL PROYECTO DEL BANCO DEL DIA 11
+ */
+
+let datosJSON;
+
+function cargarCuenta() {
+  fetch("cuentaBanco.json")
+    .then((res) => res.json())
+    .then((salida) => {
+      datosJSON = salida;
+
+      let nombre = document.getElementById("nombre");
+      let n_cuenta = document.getElementById("n_cuenta");
+      let usd = document.getElementById("usd");
+      let euros = document.getElementById("euros");
+      let CBU = document.getElementById("CBU");
+
+      nombre.textContent = datosJSON.nombre;
+      n_cuenta.textContent = datosJSON.numero_cuenta;
+      usd.textContent = datosJSON.saldo[0] + "$";
+      euros.textContent = datosJSON.saldo[1] + "€";
+      CBU.textContent = datosJSON.CBU;
+    })
+    .catch(function (error) {
+      alert(error);
+    });
+}
